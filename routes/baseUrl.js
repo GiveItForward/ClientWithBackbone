f2c4dbf47da0a1325bad10dbe8053ec2bb096e6e
missKenzington <makenzie7elliott@gmail.com>
2018-02-27 15:52:18 -0700

    fixed mistake in url

--- routes/baseUrl.js
+++ routes/baseUrl.js
@@ -2,7 +2,7 @@
 module.exports = {
 
     // tomcat_url : 'http://localhost:8080',
-    tomcat_url : 'https://54.227.151.133:8080/giveitforward',
+    tomcat_url : 'http://54.227.151.133:8080/giveitforward',
 
     // paypal_url : 'http://localhost:3000'
     paypal_url : 'https://www.giveitforward.us/'
