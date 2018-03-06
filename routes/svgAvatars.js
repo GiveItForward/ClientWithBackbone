14a641d676c70a34edc57629290788c5227e6563
missKenzington <makenzie7elliott@gmail.com>
2018-03-05 20:18:24 -0700

    changed svg url

--- routes/svgAvatars.js
+++ routes/svgAvatars.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.post('/*', function(req, res, next) {
 
     console.log("IN SVG SHIT");
-    console.log(req);
+    console.log(req.body);
     res.status(200).send(req.body);
 });
 
