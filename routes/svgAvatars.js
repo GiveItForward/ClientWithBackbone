b6cac48035e24e86f298bbf374003acfbf2c427c
missKenzington <makenzie7elliott@gmail.com>
2018-03-05 19:26:41 -0700

    changed get to post du

--- routes/svgAvatars.js
+++ routes/svgAvatars.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/*', function(req, res, next) {
+router.post('/*', function(req, res, next) {
 
     console.log("IN SVG SHIT");
     console.log(req);
