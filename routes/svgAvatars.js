f3bc5ee7a20a41297bfea73d59969c3ef6bf6053
missKenzington <makenzie7elliott@gmail.com>
2018-03-05 20:39:25 -0700

    Updated to save file to the server

--- routes/svgAvatars.js
+++ routes/svgAvatars.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 router.post('/*', function(req, res, next) {
 
+    // run php script
     console.log("IN SVG SHIT");
-    console.log(req.body);
-    res.status(200).send("saved");
+    console.log(req.body.imgdata);
+
+    fs.writeFile("/svgavatars/ready-avatars/" + req.body.filename, req.body.imgdata, function(err) {
+        if(err) {
+            return console.log(err);
+            res.status(200).send("error");
+
+        }
+
+        console.log("The file was saved!");
+        res.status(200).send("saved");
+
+    });
 });
 
 module.exports = router;
