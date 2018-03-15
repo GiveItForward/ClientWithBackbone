6674372d313acf5b6ba3908f4e4f148791e7fd47
nensimons <nensimons@gmail.com>
2018-03-15 10:26:39 -0600

    deleted unused files in routes and views

--- routes/moreHelp.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-    res.render('moreHelp', { title: 'Give It Forward' });
-});
-
-module.exports = router;
