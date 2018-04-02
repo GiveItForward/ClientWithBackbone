80f3f303b8903c6b430c90bf7388ae0e5c92f3d6
Sara Adamson <sm.adamson.pink@gmail.com>
2018-04-02 12:53:04 -0600

    New landing page, draft 1.

--- routes/baseUrl.js
+++ routes/baseUrl.js
@@ -1,11 +1,11 @@
 
 module.exports = {
 
-    // tomcat_url : 'http://localhost:8080',
-    tomcat_url : 'http://54.227.151.133:8080/giveitforward',
+    tomcat_url : 'http://localhost:8080',
+    // tomcat_url : 'http://54.227.151.13da3:8080/giveitforward',
 
-    // paypal_url : 'http://localhost:3000'
-    paypal_url : 'https://www.giveitforward.us'
+    paypal_url : 'http://localhost:3000'
+    // paypal_url : 'https://www.giveitforward.us'
 
 };
 
