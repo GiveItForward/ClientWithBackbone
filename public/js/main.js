ff8aa1018e4df5d93baaff56816a3203ed97a92e
nensimons <nensimons@gmail.com>
2018-04-03 16:54:07 -0600

    back to inline

--- public/js/main.js
+++ public/js/main.js
@@ -62,23 +62,6 @@ require.config({
 
 require(["jquery", "views/LandingView"], function ($, LandingView) {
     $(document).ready(function() {
-        function onSignIn(googleUser) {
-            // Useful data for your client-side scripts:
-            console.log('in main js');
-
-            var profile = googleUser.getBasicProfile();
-            console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-            console.log('Full Name: ' + profile.getName());
-            console.log('Given Name: ' + profile.getGivenName());
-            console.log('Family Name: ' + profile.getFamilyName());
-            console.log("Email: " + profile.getEmail());
-
-            // The ID token you need to pass to your backend:
-            var id_token = googleUser.getAuthResponse().id_token;
-            console.log("ID Token: " + id_token);
-            var view = new LandingView();
-            view.trigger('#googleIn', googleUser);
-        };
         new LandingView();
     });
 });
