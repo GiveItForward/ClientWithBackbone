ee07ad581c822d04244e60b7e23e77d44dd4025c
missKenzington <makenzie7elliott@gmail.com>
2018-04-10 18:33:06 -0600

    added google logic to node api user/create

--- routes/api.js
+++ routes/api.js
@@ -405,10 +405,14 @@ router.post('/users/create', function(req, res, next) {
 
     request(options, function(error, response, body){
         if(response.statusCode === 200){
-            var user = body;
-            session.email = user.email;
-            session.userObject = user;
-            session.cookie.maxAge = new Date(Date.now() + (60000 * 30)); // 30 minute session
+
+            if (req.headers.google == true) {
+                var user = body;
+                session.email = user.email;
+                session.userObject = user;
+                session.cookie.maxAge = new Date(Date.now() + (60000 * 30)); // 30 minute session
+            }
+
             res.send(body);
         } else {
             res.status(response.statusCode).send(body);
