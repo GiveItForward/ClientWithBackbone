b4a68433319b35bf31ee63150245aed3bff75119
Sara Adamson <sm.adamson.pink@gmail.com>
2018-04-18 19:50:19 -0600

    Added environment variables for google client id and ip address. Those will be changed now.

--- routes/baseUrl.js
+++ routes/baseUrl.js
@@ -2,7 +2,8 @@
 module.exports = {
 
     // tomcat_url : 'http://localhost:8080',
-    tomcat_url : 'http://54.227.151.133:8080/giveitforward',
+    // tomcat_url : 'http://54.227.151.133:8080/giveitforward',
+    tomcat_url: process.env.SERVER_IP + '/giveitforward',
 
     // paypal_url : 'http://localhost:3000'
     paypal_url : 'https://www.giveitforward.us'
